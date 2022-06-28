1b89b66b7c6bb4637547f728ff8adaec1b6bb0e7
Zeljko Dujmovic <zeljko.dujmovic83@gmail.com>
2022-06-28 16:40:36 +0200

    TASK-7: Add theme but unit tests not working

--- mobile/android/src/main/java/ch/levelup/kaesseli/android/MainActivity.kt
+++ mobile/android/src/main/java/ch/levelup/kaesseli/android/MainActivity.kt
@@ -3,16 +3,18 @@ package ch.levelup.kaesseli.android
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.layout.Arrangement.Center
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import ch.levelup.kaesseli.Greeting
+import ch.levelup.kaesseli.android.ui.theme.JetpackComposeTestTheme
 
 fun greet(): String {
     return Greeting().greeting()
@@ -22,8 +24,14 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MaterialTheme {
-                MainView()
+            JetpackComposeTestTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    MainView()
+                }
             }
         }
     }
@@ -38,5 +46,24 @@ fun MainView() {
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
         Text(text = greet())
+        Button(
+            onClick = { /* ... */ },
+            // Uses ButtonDefaults.ContentPadding by default
+            contentPadding = PaddingValues(
+                start = 20.dp,
+                top = 12.dp,
+                end = 20.dp,
+                bottom = 12.dp
+            )
+        ) {
+            // Inner content including an icon and a text label
+            Icon(
+                Icons.Filled.Favorite,
+                contentDescription = "Favorite",
+                modifier = Modifier.size(ButtonDefaults.IconSize)
+            )
+            Spacer(Modifier.size(ButtonDefaults.IconSpacing))
+            Text("Like")
+        }
     }
 }
