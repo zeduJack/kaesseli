3556618f1381c1a4c3f3eafed9495c9060dd6979
Choesang Tenzin <tchoesang@gmail.com>
2022-11-13 14:37:59 +0100

    TASK-5 Configure to run in azure cloud

--- backend/src/main/kotlin/ch/levelup/kaesseli/backend/transaction/Transaction.kt
+++ backend/src/main/kotlin/ch/levelup/kaesseli/backend/transaction/Transaction.kt
@@ -1,14 +1,12 @@
 package ch.levelup.kaesseli.backend.transaction
 
-import ch.levelup.kaesseli.backend.account.Account
 import ch.levelup.kaesseli.backend.common.TransactionStatus
-import ch.levelup.kaesseli.backend.user.User
 import java.math.BigDecimal
 import java.time.LocalDateTime
-import javax.persistence.GeneratedValue
-import javax.persistence.GenerationType
-import javax.persistence.Id
+import javax.persistence.*
 
+@Entity
+@Table(name = "\"transaction\"")
 data class Transaction(
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -18,8 +16,8 @@ data class Transaction(
     val version: Int,
 
     val amount: BigDecimal,
-    val user: User,
-    val account: Account,
+    //val user: User,
+    //val account: Account,
     val debit: Boolean,
     val status: TransactionStatus,
     val message: String
