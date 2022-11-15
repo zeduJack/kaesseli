ac2a11c5eeddc18f9bc3158c8425bb397854908a
Sarah <svplo@gmx.de>
2022-11-15 18:46:29 +0100

    update color theme

--- mobile/android/src/main/java/ch/levelup/kaesseli/android/ui/theme/Color.kt
+++ mobile/android/src/main/java/ch/levelup/kaesseli/android/ui/theme/Color.kt
@@ -2,7 +2,15 @@ package ch.levelup.kaesseli.android.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple200 = Color(0xFFBB86FC)
-val Purple500 = Color(0xFF6200EE)
-val Purple700 = Color(0xFF3700B3)
-val Teal200 = Color(0xFF03DAC5)
+//val Purple200 = Color(0xFFBB86FC)
+//val Purple500 = Color(0xFF6200EE)
+//val Purple700 = Color(0xFF3700B3)
+//val Teal200 = Color(0xFF03DAC5)
+val CristalBlue = Color(0xFF5C95A5)
+val Blue = Color(0xFF1897D5)
+val Grey = Color(0xFF616060)
+val Anthracite = Color(0xFF474545)
+val White = Color(0xFFE6E6E6)
+val Turquoise = Color(0xFF13A0A5)
+val LightBlue = Color(0xFF1B9AD8)
+val LightGrey = Color(0xFF8D8D8D)
