37a5e6ea7afe9038dd2aa95b91cc433b0cf5693f
Choesang Tenzin <tchoesang@gmail.com>
2023-01-23 12:48:04 +0100

    TASK-40 Firebase push notification
    Google credential stored as base64. It is read from kubernetes secret, decoded to plain text and loaded into google credential object

--- backend/src/main/kotlin/ch/levelup/kaesseli/backend/BackendApplication.kt
+++ backend/src/main/kotlin/ch/levelup/kaesseli/backend/BackendApplication.kt
@@ -1,69 +1,26 @@
 package ch.levelup.kaesseli.backend
 
-import com.google.auth.oauth2.GoogleCredentials
-import com.google.firebase.FirebaseApp
-import com.google.firebase.FirebaseOptions
-import org.slf4j.LoggerFactory
+import ch.levelup.kaesseli.backend.firebase.Firebase
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.stereotype.Service
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RestController
+import java.util.*
 
 
 @SpringBootApplication
 class BackendApplication
 
 fun main(args: Array<String>) {
+	Firebase().initiateFirebase()
 	runApplication<BackendApplication>(*args)
-	//initiateFirebase()
 }
-
-private fun initiateFirebase() {
-	val options = FirebaseOptions.builder()
-		.setCredentials(GoogleCredentials.getApplicationDefault())
-		.setProjectId("kaesseli-18cf8")
-		.build()
-
-	FirebaseApp.initializeApp(options)
-}
-
-
-
 
 @RestController
-class MessageResource(val service: MessageService) {
+class MessageResource(val service: Firebase) {
 	@PostMapping("token/{token}")
-	fun sendMessage(@PathVariable token: String) {
-		service.sendMessage(token)
-	}
-}
-
-
-@Service
-class MessageService() {
-
-	private val LOG = LoggerFactory.getLogger(this::class.java)
-
-	fun sendMessage(token: String) {
-		LOG.warn("Token received {}", token)
-		Firebase().pushMessage(token)
-	}
-}
-
-@Service
-class FirebaseUserManagement() {
-
-	private val LOG = LoggerFactory.getLogger(this::class.java)
-
-	fun deleteFirebaseUser(uid: String): Boolean {
-		val success = Firebase().deleteFirebaseUser(uid)
-		LOG.debug("Deleted user with UID: $uid. Status: $success")
-		return success
-	}
-
-	fun logOutUser(uid: String) {
-		Firebase().revokeFirebaseToken(uid)
+	fun sendMessage(@PathVariable token: String, title: String, body: String) {
+		service.pushMessage(token, title, body)
 	}
 }
---
+++ backend/src/main/kotlin/ch/levelup/kaesseli/backend/firebase/Firebase.kt
@@ -0,0 +1,97 @@
+package ch.levelup.kaesseli.backend.firebase
+
+import com.google.auth.oauth2.GoogleCredentials
+import com.google.firebase.FirebaseApp
+import com.google.firebase.FirebaseOptions
+import com.google.firebase.auth.*
+import com.google.firebase.messaging.FirebaseMessaging
+import com.google.firebase.messaging.Message
+import com.google.firebase.messaging.Notification
+import org.slf4j.LoggerFactory
+import org.springframework.stereotype.Service
+import java.util.*
+
+@Service
+class Firebase {
+    private val logger = LoggerFactory.getLogger(this::class.java)
+
+    fun initiateFirebase() {
+        val encodedToken = System.getenv("FIREBASE_TOKEN")
+        val decodedToken = String(Base64.getDecoder().decode(encodedToken))
+        val credential = GoogleCredentials.fromStream(decodedToken.byteInputStream())
+
+        val options = FirebaseOptions.builder()
+                .setCredentials(credential)
+                .setProjectId("kaesseli-18cf8")
+                .build()
+
+        FirebaseApp.initializeApp(options)
+    }
+
+    fun pushMessage(token: String, title: String, body: String): String? {
+        val notification = Notification.builder()
+                .setTitle(title)
+                .setBody(body)
+                .build()
+        val message = Message.builder()
+                .setNotification(notification)
+                .setToken(token)
+                .build()
+        return FirebaseMessaging.getInstance().send(message)
+    }
+
+    fun createFirebaseUser(firebaseUid: String, email: String, pwd: String) {
+        // https://firebase.google.com/docs/auth/admin/errors
+        val firebaseAuth = FirebaseAuth.getInstance()
+        try {
+            firebaseAuth.getUser(firebaseUid)
+            firebaseAuth.deleteUser(firebaseUid)
+            println("Successfully deleted user: $firebaseUid")
+        } catch (e: FirebaseAuthException) {
+            println("User does not exist: $firebaseUid")
+        }
+        val request: UserRecord.CreateRequest = UserRecord.CreateRequest()
+                .setUid(firebaseUid)
+                .setEmail(email)
+                .setPassword(pwd)
+                .setDisabled(false)
+        val userRecord = firebaseAuth.createUser(request)
+        if (firebaseUid != userRecord.uid) {
+            println("Error, unexpected user created")
+        }
+        println("Successfully created new user: " + userRecord.uid)
+    }
+
+    fun deleteFirebaseUser(uid: String): Boolean {
+        val success = Firebase().deleteFirebaseUser(uid)
+        logger.debug("Deleted user with UID: $uid. Status: $success")
+        return success
+    }
+
+    fun logOutUser(uid: String) {
+        Firebase().revokeFirebaseAccessToken(uid)
+    }
+
+    private fun verifyFirebaseIdToken(idToken: String): String? {
+        try {
+            // idToken comes from the client app see also https://firebase.google.com/docs/auth/admin/verify-id-tokens#java
+            val decodedToken: FirebaseToken = FirebaseAuth.getInstance().verifyIdToken(idToken, true)
+            return decodedToken.getUid()
+        } catch (e: FirebaseAuthException) {
+            if (e.getAuthErrorCode() == AuthErrorCode.REVOKED_ID_TOKEN) {
+                println("Token has been revoked. Inform the user to re-authenticate or signOut() the user.")
+            } else {
+                println("Token is invalid.")
+            }
+            return null
+        }
+    }
+
+    private fun revokeFirebaseAccessToken(uid: String) {
+        FirebaseAuth.getInstance().revokeRefreshTokens(uid)
+        val user = FirebaseAuth.getInstance().getUser(uid)
+        // Convert to seconds as the auth_time in the token claims is in seconds too.
+        val revocationSecond = user.tokensValidAfterTimestamp / 1000
+        println("Tokens revoked at: $revocationSecond")
+    }
+}
